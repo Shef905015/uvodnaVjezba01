07c4c18e84c6f28bd47d424dd32f0cce2e79a0a5
Non't <90344104+Shef905015@users.noreply.github.com>
2023-09-20 08:42:58 +0200

    Add files via upload

---
+++ VarijableIspisPodataka/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarijableIspisPodataka
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 3;
+            int x = a / b;
+            int y = a % b;
+
+
+            Console.WriteLine(a + " dijeljeno sa " + b + " je " + x + " s ostatkom " + y);
+
+            Console.ReadKey();
+        }
+    }
+}
